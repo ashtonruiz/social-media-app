eb4e8491553423eece066917f6e3901a00230c0a
Harold Jean <haroldjean38@yahoo.com>
2022-12-07 21:27:33 -0500

    added my supabase to fetch

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = 'https://zgixhmlshitskkemwyaf.supabase.co';
+const SUPABASE_URL = 'https://madsmicxvvkhsnmoovxs.supabase.co';
 const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnaXhobWxzaGl0c2trZW13eWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgxMDgwODYsImV4cCI6MTk4MzY4NDA4Nn0.nMjJ-vp1PSZuD_oT9AQGKADmPu3OCZp9Uf4n2XbaBjQ';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hZHNtaWN4dnZraHNubW9vdnhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAyNjIwNzUsImV4cCI6MTk4NTgzODA3NX0.jWpWr5EbuVoJ5zcPGAuIulwzNcM674ZT6agUtX1McPE';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
